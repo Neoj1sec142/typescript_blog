1ba92d4968b19bd1e301e072b49f61822e585b56
Mark Harmon <neothej1sec@gmail.com>
2022-10-10 14:58:06 -0500

    setting up interfaces for hhtp reqs

--- client/src/componenets/interfaces/index.tsx
+++ client/src/componenets/interfaces/index.tsx
@@ -6,3 +6,12 @@ export interface User{
     first_name: string,
     last_name: string
 }
+
+// user title  content img_url  date_created  date_modified 
+export interface Post{
+    id: number,
+    user: number,
+    content: string,
+    img_url: string, 
+    date_created: Date,
+}
